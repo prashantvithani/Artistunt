33d0913e3d028b3dddcde4f0edda6a0d48eb856d
Prashant Vithani <prashantvithani@gmail.com>
2015-07-26 07:43:09 +0530

    Store previous location | Set cache buster

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,28 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_filter :set_cache_buster
+  after_filter :store_location
+
+	def store_location
+	  # store last url - this is needed for post-login redirect to whatever the user last visited.
+	  return unless request.get?
+	  if (request.path != "/users/sign_in" &&
+	      request.path != "/users/sign_up" &&
+	      request.path != "/users/password/new" &&
+	      request.path != "/users/password/edit" &&
+	      request.path != "/users/confirmation" &&
+	      request.path != "/users/sign_out" &&
+	      !request.xhr?) # don't store ajax calls
+	    session[:previous_url] = request.fullpath
+	  end
+	end
+
+	def after_sign_in_path_for(resource)
+	  session[:previous_url] || root_path
+	end
 
   protected
 
@@ -11,4 +32,10 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:username, :password, :remember_me) }
     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password) }
   end
+
+	def set_cache_buster
+	    response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+	    response.headers["Pragma"] = "no-cache"
+	    response.headers["Expires"] = "#{1.year.ago}"
+  	end
 end
